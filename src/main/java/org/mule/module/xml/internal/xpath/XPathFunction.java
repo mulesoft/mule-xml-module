866835ecd6939a40eba66679c975d823726d9049
Alejandro Curci <76498083+acurci-at-mulesoft@users.noreply.github.com>
2021-03-03 11:27:27 -0300

    XMLM-16: Added namespace parameter to xpath function (#88)
    * Added new paremeter in xpath function for providing namespaces
    
    * Added new paremeter in xpath function for providing namespaces
    
    * XMLM-16: Changed visibility of NamespaceMapping's attributes to protected and remove accesing by reflection
    
    * XMLM-16: Changed visibility of NamespaceMapping's attributes to protected and remove inner class
    
    * XMLM-16: Refactor test case
    
    * XMLM-16: Refactor test case
    
    * XMLM-16: Removed unused logger and imports
    
    * XMLM-16: Restored visibility for NamespaceMapping to private

--- src/main/java/org/mule/module/xml/internal/xpath/XPathFunction.java
+++ src/main/java/org/mule/module/xml/internal/xpath/XPathFunction.java
@@ -6,8 +6,9 @@
  */
 package org.mule.module.xml.internal.xpath;
 
-import static java.util.Collections.emptyList;
 import static org.mule.module.xml.api.EntityExpansion.NEVER;
+
+import org.mule.module.xml.api.NamespaceMapping;
 import org.mule.module.xml.internal.XmlModule;
 import org.mule.module.xml.internal.operation.XPathOperation;
 import org.mule.runtime.api.exception.MuleException;
@@ -15,8 +16,11 @@ import org.mule.runtime.api.lifecycle.Initialisable;
 import org.mule.runtime.api.lifecycle.InitialisationException;
 import org.mule.runtime.api.lifecycle.Startable;
 import org.mule.runtime.api.lifecycle.Stoppable;
+import org.mule.runtime.extension.api.annotation.param.Optional;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -29,6 +33,7 @@ public class XPathFunction implements Initialisable, Startable, Stoppable {
 
   // By default, remove trailing newlines, to be backward compatible with previous xml-module versions.
   private static final boolean DEFAULT_KEEP_TRAILING_NEWLINES_CONFIGURATION = false;
+
   private XPathOperation xpathOperation;
   private XmlModule config = new XmlModule();
 
@@ -53,11 +58,16 @@ public class XPathFunction implements Initialisable, Startable, Stoppable {
    *
    * @param xpath the XPath script
    * @param content the XML content on which the XPath is evaluated
-   * @param contextProperties Properties that wil be made available to the transform context.
+   * @param contextProperties Properties that will be made available to the transform context.
+   * @param ns NamespaceMapping Maps a prefix to a namespace URI
    * @return a List of Strings with all the matching elements
    */
-  public List<String> xpath(String xpath, InputStream content, Map<String, Object> contextProperties) {
-    return xpathOperation.xpathExtract(content, xpath, contextProperties, emptyList(), null, config,
+  public List<String> xpath(String xpath, InputStream content, Map<String, Object> contextProperties,
+                            @Optional NamespaceMapping... ns) {
+    List<NamespaceMapping> namespaces = new ArrayList<>();
+    if (ns != null)
+      namespaces.addAll(Arrays.asList(ns));
+    return xpathOperation.xpathExtract(content, xpath, contextProperties, namespaces, null, config,
                                        DEFAULT_KEEP_TRAILING_NEWLINES_CONFIGURATION);
   }
 }
